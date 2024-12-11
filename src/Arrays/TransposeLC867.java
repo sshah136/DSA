c6e9ae6884f6eaa9146b469928fe560cbd8d114f
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-12-11 15:24:50 -0500

    Update TransposeLC867.java

--- src/Arrays/TransposeLC867.java
+++ src/Arrays/TransposeLC867.java
@@ -1,4 +1,18 @@
 package Arrays;
 
 public class TransposeLC867 {
+    public int[][] transpose(int[][] matrix) {
+
+        int[][] output = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < output.length; i++)
+        {
+            for (int j = 0; j < output[0].length; j++)
+            {
+                output[i][j] = matrix[j][i];
+            }
+        }
+        return output;
+
+    }
 }
