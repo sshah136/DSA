157a7d0d9ea68ce4f1e6b76ae78414619672b47b
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-12-18 17:29:59 -0500

    Alternate solution

--- src/Arrays/MergeSortedArrayLC88.java
+++ src/Arrays/MergeSortedArrayLC88.java
@@ -24,4 +24,23 @@ public class MergeSortedArrayLC88 {
 
         }
     }
+    // 2nd approach
+
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = nums1.length-1;
+
+        while(k>=0){
+            if(j<0){
+                break;
+            }
+            if(i>=0 && nums1[i] >= nums2[j]){ // 'i' cannot reach <0 before k. even if nums1[i] will be exhausted,
+                // it'll stay at 0 till nums2[j] is completed
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
 }
